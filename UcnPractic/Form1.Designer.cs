c0e43d42d7c9c2a0bbb4f35c697ab798f2bad637
Seniy_Jalilow Seniy_Jalilow <arseniy-3108@mail.ru>
2023-12-12 22:56:34 +0500

    Добавьте файлы проекта.

---
+++ UcnPractic/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace UcnPractic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOth = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtNoum = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Calendar = new System.Windows.Forms.MonthCalendar();
+            this.BoxKlass = new System.Windows.Forms.ComboBox();
+            this.BoxChildren = new System.Windows.Forms.ComboBox();
+            this.txtData = new System.Windows.Forms.MaskedTextBox();
+            this.checkedList = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboReis = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(179, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Регистрация";
+            // 
+            // txtFam
+            // 
+            this.txtFam.Location = new System.Drawing.Point(103, 76);
+            this.txtFam.Name = "txtFam";
+            this.txtFam.Size = new System.Drawing.Size(164, 20);
+            this.txtFam.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(31, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Фамилия:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(31, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Отчество:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(31, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Имя:";
+            // 
+            // txtOth
+            // 
+            this.txtOth.Location = new System.Drawing.Point(103, 132);
+            this.txtOth.Name = "txtOth";
+            this.txtOth.Size = new System.Drawing.Size(164, 20);
+            this.txtOth.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(103, 105);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblName.Location = new System.Drawing.Point(31, 159);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(70, 16);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Телефон:";
+            // 
+            // txtNoum
+            // 
+            this.txtNoum.Location = new System.Drawing.Point(104, 158);
+            this.txtNoum.Mask = "+7(000)-00-0000";
+            this.txtNoum.Name = "txtNoum";
+            this.txtNoum.Size = new System.Drawing.Size(163, 20);
+            this.txtNoum.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(438, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Дата:";
+            // 
+            // Calendar
+            // 
+            this.Calendar.Location = new System.Drawing.Point(486, 105);
+            this.Calendar.Name = "Calendar";
+            this.Calendar.TabIndex = 13;
+            this.Calendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.Calendar_DateChanged);
+            // 
+            // BoxKlass
+            // 
+            this.BoxKlass.FormattingEnabled = true;
+            this.BoxKlass.Items.AddRange(new object[] {
+            "Эконом",
+            "Комфорт",
+            "Бизнес",
+            "Первый класс"});
+            this.BoxKlass.Location = new System.Drawing.Point(273, 106);
+            this.BoxKlass.Name = "BoxKlass";
+            this.BoxKlass.Size = new System.Drawing.Size(159, 21);
+            this.BoxKlass.TabIndex = 14;
+            // 
+            // BoxChildren
+            // 
+            this.BoxChildren.FormattingEnabled = true;
+            this.BoxChildren.Items.AddRange(new object[] {
+            "Без детей ",
+            "С детьми"});
+            this.BoxChildren.Location = new System.Drawing.Point(273, 75);
+            this.BoxChildren.Name = "BoxChildren";
+            this.BoxChildren.Size = new System.Drawing.Size(159, 21);
+            this.BoxChildren.TabIndex = 15;
+            this.BoxChildren.SelectedIndexChanged += new System.EventHandler(this.BoxChildren_SelectedIndexChanged);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(486, 73);
+            this.txtData.Mask = "00/00/0000";
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(163, 20);
+            this.txtData.TabIndex = 16;
+            this.txtData.ValidatingType = typeof(System.DateTime);
+            // 
+            // checkedList
+            // 
+            this.checkedList.FormattingEnabled = true;
+            this.checkedList.Items.AddRange(new object[] {
+            "Багаж",
+            "Обратный билет"});
+            this.checkedList.Location = new System.Drawing.Point(273, 135);
+            this.checkedList.Name = "checkedList";
+            this.checkedList.Size = new System.Drawing.Size(159, 34);
+            this.checkedList.TabIndex = 24;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(407, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 40);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Купить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboReis
+            // 
+            this.comboReis.FormattingEnabled = true;
+            this.comboReis.Items.AddRange(new object[] {
+            "Екатеринбург >Санкт-Петербург",
+            "Москва --> Красноярск",
+            "Екатеринбург > Варшава",
+            "Санкт-Петербург> Стамбул",
+            "Новосибирск>Дубаи"});
+            this.comboReis.Location = new System.Drawing.Point(131, 205);
+            this.comboReis.Name = "comboReis";
+            this.comboReis.Size = new System.Drawing.Size(159, 21);
+            this.comboReis.TabIndex = 45;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(31, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Выбор рейса:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(214, 285);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 47;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(31, 285);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 16);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Кол-во детских билетов:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(31, 249);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(177, 16);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Кол-во взрослых билетов:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(214, 249);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 49;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(590, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 40);
+            this.button3.TabIndex = 51;
+            this.button3.Text = "Отчистить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(670, 322);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboReis);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedList);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.BoxChildren);
+            this.Controls.Add(this.BoxKlass);
+            this.Controls.Add(this.Calendar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNoum);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtOth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFam);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Билеты";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOth;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.MaskedTextBox txtNoum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MonthCalendar Calendar;
+        private System.Windows.Forms.ComboBox BoxKlass;
+        private System.Windows.Forms.ComboBox BoxChildren;
+        private System.Windows.Forms.MaskedTextBox txtData;
+        private System.Windows.Forms.CheckedListBox checkedList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboReis;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ UcnPractic/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace UcnPractic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BoxChildren_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (BoxChildren.SelectedItem != null && BoxChildren.SelectedItem.ToString() == "С детьми")
+            {
+                label7.Visible = true;
+                numericUpDown1.Visible = true;
+            }
+            else
+            {
+                label7.Visible = false;
+                numericUpDown1.Visible = false;
+            }
+        }
+
+        private void Calendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            txtData.Text = Calendar.SelectionStart.ToShortDateString();
+        }
+        private int button1Clicks = 0;
+        public void button2_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.aviasales.ru/?marker=15468.gaasru8877gclidCjwKCAiA98WrBhAYEiwA2WvhOtrw3SzZQXPzEYGBLzWbWeYNxJnh3zQCUYRFem3tlDLyZUk-i8B_pxoCvPAQAvD_BwEgclid&gad_source=1&gclid=CjwKCAiA98WrBhAYEiwA2WvhOtrw3SzZQXPzEYGBLzWbWeYNxJnh3zQCUYRFem3tlDLyZUk-i8B_pxoCvPAQAvD_BwE&params=AMS1";
+
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при открытии ссылки: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Проверяем, заполнены ли все поля
+            if (!string.IsNullOrEmpty(txtFam.Text) &&
+                !string.IsNullOrEmpty(txtName.Text) &&
+                !string.IsNullOrEmpty(txtOth.Text) &&
+                !string.IsNullOrEmpty(txtNoum.Text) &&
+                !string.IsNullOrEmpty(txtData.Text) &&
+                checkedList.CheckedItems.Count > 0)
+            {
+                // Увеличиваем счетчик нажатий кнопки и обновляем метку
+                button1Clicks++;
+                
+            }
+            else
+            {
+                // Выводим сообщение об ошибке, если не все поля заполнены
+                MessageBox.Show("Заполните все поля перед обновлением", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            double totalCost = 0;
+
+            // Проверка выбранного направления в comboReis
+            string selectedRoute = comboReis.SelectedItem.ToString();
+            if (selectedRoute == "Екатеринбург >Санкт-Петербург")
+            {
+                totalCost += 1500;
+            }
+            else if (selectedRoute == "Москва > Красноярск" || selectedRoute == "Екатеринбург > Варшава")
+            {
+                totalCost += 2000;
+            }
+            else if (selectedRoute == "Санкт-Петербург > Стамбул")
+            {
+                totalCost += 3000;
+            }
+            else if (selectedRoute == "Новосибирск > Дубаи")
+            {
+                totalCost += 3200;
+            }
+
+            // Проверка выбранных опций
+            if (checkedList.CheckedItems.Contains("Багаж"))
+            {
+                totalCost += 300;
+            }
+            if (checkedList.CheckedItems.Contains("Обратный билет"))
+            {
+                totalCost *= 2;
+            }
+            if (BoxChildren.SelectedItem != null && BoxChildren.SelectedItem.ToString() == "С детьми")
+            {
+                totalCost += 1500 * (int)numericUpDown1.Value;
+            }
+
+            // Проверка выбранного класса в BoxKlass
+            string selectedClass = BoxKlass.SelectedItem.ToString();
+            if (selectedClass == "Комфорт")
+            {
+                totalCost += 300;
+            }
+            else if (selectedClass == "Бизнес")
+            {
+                totalCost += 2000;
+            }
+            else if (selectedClass == "Первый класс")
+            {
+                totalCost += 5000;
+            }
+
+            // Добавление стоимости за количество детей и дополнительный багаж из numericUpDown1 и numericUpDown2
+            totalCost += 2500 * (int)numericUpDown2.Value;
+
+            // Отображение окна с итоговой стоимостью
+            MessageBox.Show($"Итоговая стоимость билета: {totalCost} рублей", "Стоимость билета", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Элементы по умолчанию
+            BoxChildren.SelectedItem = "С детьми";
+            BoxKlass.SelectedItem = "Эконом";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtFam.Text = " ";
+            txtName.Text = " ";
+            txtOth.Text = " ";
+            txtNoum.Text = " ";
+            txtData.Text = " ";
+            comboReis.Text = " ";
+            numericUpDown2.Value = 0;
+            numericUpDown1.Value = 0;
+            BoxKlass.Text = "Эконом";
+        }
+    }
+}
